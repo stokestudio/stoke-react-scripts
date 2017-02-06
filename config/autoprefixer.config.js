cbdac30a9b63b7efa1b5aef00f1681aef0bddf13
Kevin Wade <kevin@kevinwade.name>
2017-02-06 10:22:47 -0600

    Add comment about where to test browser versions config

--- config/autoprefixer.config.js
+++ config/autoprefixer.config.js
@@ -1,3 +1,6 @@
+// Test browser versions here:
+// http://browserl.ist/?q=last+2+versions%2C+last+2+ChromeAndroid+versions%2C+not+IE+%3C+11%2C+not+ExplorerMobile+%3E%3D+0
+
 module.exports = {
   browsers: [
     'last 2 versions',
